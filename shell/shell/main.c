4f856aa346ac70f1f0eca2bb81a15208ee645802
Виталий Давыдов <iwitaly@me.com>
2013-05-13 20:12:09 +0400

    creating

---
+++ shell/shell/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  shell
+//
+//  Created by Виталий Давыдов on 13.05.13.
+//  Copyright (c) 2013 Виталий Давыдов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    return 0;
+}
+
